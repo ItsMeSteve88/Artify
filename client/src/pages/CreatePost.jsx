afb1158ad65cd9a0c1363cc53672643130dac50e
Stephen James <78153905+ItsMeSteve88@users.noreply.github.com>
2023-05-16 17:48:27 +0100

    server issues

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -44,7 +44,34 @@ const CreatePost = () =>
       alert('Please enter a prompt')
     }
   }
-  const handleSubmit = () => {}
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ ...form }),
+        });
+
+        await response.json();
+        alert('Success');
+        navigate('/');
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please generate an image with proper details');
+    }
+  };
+  
   const handleChange = (e) =>
   {
     setForm({...form, [e.target.name]: e.target.value})
@@ -93,7 +120,8 @@ const CreatePost = () =>
                 alt={form.prompt}
                 className='w-full h-full object-contain'
               />
-            ) : (<img
+            ) : (
+              <img
                 src={preview}
                 alt='preview'
                 className='w-9/12 h-9/12 object-contain opacity-40'
